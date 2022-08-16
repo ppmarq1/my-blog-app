ca2e9cba31718d7161fad94d86c320d858d5d490
Pedro Marques <ppmarq1@gmail.com>
2022-08-16 19:09:23 +0100

    Generate migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_16_173958) do
+ActiveRecord::Schema[7.0].define(version: 20_220_816_173_958) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "photo"
-    t.string "bio"
-    t.integer "post_counter"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_users_on_name"
+  create_table 'users', force: :cascade do |t|
+    t.string 'name'
+    t.string 'photo'
+    t.string 'bio'
+    t.integer 'post_counter'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['name'], name: 'index_users_on_name'
   end
 end
