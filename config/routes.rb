017544ae0ac0bc916179ad3e061ffeadaacfc3bd
Pedro Marques <ppmarq1@gmail.com>
2022-08-25 09:22:06 +0100

    Add controllers, views and routes to form

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ('/')
-  root 'users#index'
+  # Defines the root path route ("/")
+  root "users#index"
 
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
+    resources :posts, only: %i[index new create show] do
+    end
+  end
+
+  resources :posts do
+    resources :comments, only: %i[new, create]
+    resources :likes, only: %i[create]
   end
 end
