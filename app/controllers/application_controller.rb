fec490b31800f8605246377c82ef300c2bcb58e2
Pedro Marques <ppmarq1@gmail.com>
2022-08-31 18:09:28 +0100

    Fix linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery prepend: true
 
-  before_action :authenticate_user!
+  before_action :authenticate_user!, unless: :api_path
 
   before_action :update_allowed_parameters, if: :devise_controller?
 
@@ -9,8 +9,8 @@ class ApplicationController < ActionController::Base
 
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :bio, :email, :password, :password_confirmation) }
-    devise_parameter_sanitizer.permit(:account_update) { |u|
+    devise_parameter_sanitizer.permit(:account_update) do |u|
       u.permit(:name, :bio, :email, :password, :current_password)
-    }
+    end
   end
 end
