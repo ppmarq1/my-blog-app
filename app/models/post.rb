d199cf2b7f023a15d056021e1f7c4f5ac19479a3
Pedro Marques <ppmarq1@gmail.com>
2022-09-01 18:42:17 +0100

    Update views and routes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :comments
-  has_many :likes
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
@@ -13,9 +13,5 @@ class Post < ApplicationRecord
 
   def most_recent_comments
     comments.order(created_at: :desc).limit(5)
-  end
-
-  def as_json(_options = {})
-    { id:, title:, text: }
   end
 end
