74b1b54b15114a70c3ac9e5e626523546bddf3e3
Pedro Marques <ppmarq1@gmail.com>
2022-09-02 11:11:27 +0100

    Add API endpoint

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,4 +5,8 @@ class Comment < ApplicationRecord
   def update_comments_posts_counter
     post.increment!(:comments_counter)
   end
+
+  def as_json(_options = {})
+    { id:, text:, post_id: }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  devise_for :users
   # Defines the root path route ("/")
   root 'users#index'
 
@@ -10,8 +11,17 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :posts, only: [:new, :create, :update, :destroy] do
+  resources :posts, only: %i[new create update destroy] do
     resources :comments
     resources :likes
+  end
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      post 'users/sign_in' => 'users#login'
+      get 'posts' => 'posts#index'
+      get 'comments' => 'comments#index'
+      post 'comments/create' => 'comments#create'
+    end
   end
 end
